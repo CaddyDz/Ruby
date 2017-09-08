27d5c6f9451b6ed01e1b6c0660de985d89cf45fc
Caddy Dz <tbitw31@gmail.com>
2017-09-08 14:56:55 +0100

    Multiline string
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- rubyOrNotRuby.rb
+++ rubyOrNotRuby.rb
@@ -1,2 +1,6 @@
-puts "Add Them #{4 + 5} \n\n"
-puts 'Add Them #{4 + 5} \n\n'
+multiline_string = <<EOM
+This is a very long string
+that contains interpolation
+like #{4 + 5} \n\n
+EOM
+puts multiline_string
