c78afcc07a0edf2bd6ba73860b8fdc63aa464206
Caddy Dz <tbitw31@gmail.com>
2017-09-08 12:58:29 +0100

    Conditional & logical operators
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- rubyOrNotRuby.rb
+++ rubyOrNotRuby.rb
@@ -1,3 +1,10 @@
-=begin
-    Multi line comment
+age = 12
+
+if (age >= 5) && (age <= 6)
+    puts "You're in Kindergarten"
+elsif (age >= 7) && (age <= 13)
+    puts "You're in Middle School"
+    puts "Yeah"
+else
+    puts "Stay Home"
 end
