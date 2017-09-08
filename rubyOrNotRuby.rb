12be7be8dde981d5dd05b55bebea6b02bd2cdbd2
Caddy Dz <tbitw31@gmail.com>
2017-09-08 13:03:25 +0100

    Logical operators in action
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- rubyOrNotRuby.rb
+++ rubyOrNotRuby.rb
@@ -1,10 +1,6 @@
 age = 12
 
-if (age >= 5) && (age <= 6)
-    puts "You're in Kindergarten"
-elsif (age >= 7) && (age <= 13)
-    puts "You're in Middle School"
-    puts "Yeah"
-else
-    puts "Stay Home"
-end
+puts "true && false = " + (true && false).to_s
+puts "true || false = " + (true || false).to_s
+puts "true ^ false = " + (true ^ false).to_s
+puts "!false = " + (!false).to_s
