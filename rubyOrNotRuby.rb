2a7bdc41b35958095fbc0fa58daf037d6683d757
Caddy Dz <tbitw31@gmail.com>
2017-09-08 15:57:26 +0100

    Trimming & formatting
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- rubyOrNotRuby.rb
+++ rubyOrNotRuby.rb
@@ -12,3 +12,13 @@ puts "\"a\".equal?(\"a\") : " + ("a".equal?"a").to_s
 puts full_name.upcase
 puts full_name.downcase
 puts full_name.swapcase
+
+full_name = "       " + full_name
+
+full_name = full_name.lstrip
+full_name = full_name.rstrip
+full_name = full_name.strip
+
+puts full_name.rjust(20, '.')
+puts full_name.ljust(20, '.')
+puts full_name.center(20, '.')
