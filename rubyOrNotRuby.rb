94fc0231594145a20498c31146e6a6e629114976
Caddy Dz <tbitw31@gmail.com>
2017-09-08 18:57:38 +0100

    Finishing up I/O
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- rubyOrNotRuby.rb
+++ rubyOrNotRuby.rb
@@ -1,14 +1,14 @@
-file = File.new("authors.out", "w")
+file = File.new("authors_info.out", "w")
 
-file.puts "William Shakespeare"
-file.puts "Agatha Christie"
-file.puts "Barbara Cartland"
-
+file.puts "William Shakespear,English,plays and poetry,4 billion"
+file.puts "Agatha Christie,English,who done its,4 billion"
+file.puts "Barbara Cartland,English,romance novels,1 billion"
+file.puts "Danielle Steel,English,romance novels,800 million"
 file.close
 
-puts File.read("authors.out")
-
-file = File.new("authors.out", "a")
-file.puts "Danielle Steel"
-file.close
-puts File.read("authors.out")
+File.open("authors_info.out") do |record|
+    record.each do |item|
+        name, lang, specialty, sales = item.chomp.split(',')
+        puts "#{name} was an #{lang} author that specialize in #{specialty}. They sold over #{sales} books"
+    end
+end
