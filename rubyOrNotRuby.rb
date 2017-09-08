88b0882e42fc0f48259bffc9e3fabf7e7ac1bb48
Caddy Dz <tbitw31@gmail.com>
2017-09-08 15:49:20 +0100

    String comparison
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- rubyOrNotRuby.rb
+++ rubyOrNotRuby.rb
@@ -7,14 +7,4 @@ middle_name = "Justin"
 
 full_name = "{first_name} #{middle_name} #{last_name}"
 
-puts full_name.include?("Justin")
-
-puts full_name.size
-
-puts "Vowels : " + full_name.count("aeiou").to_s
-
-puts "Consonants : " + full_name.count("^aeiou").to_s
-
-puts full_name.start_with?("Banas")
-
-puts "Index : " + full_name.index("Banas").to_s
+puts "a == a " + ("a" == "a").to_s
