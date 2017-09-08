900a4c711924fae6da2055d0a36daed6c9618e3b
Caddy Dz <tbitw31@gmail.com>
2017-09-08 17:12:53 +0100

    Multi dimensional hashes
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- rubyOrNotRuby.rb
+++ rubyOrNotRuby.rb
@@ -3,3 +3,17 @@ number_hash = {"PI" => 3.14,
                 "e" => 2.718}
 
 puts number_hash["PI"]
+
+superHeroes = Hash["Clark Kent", "Superman", "Bruce Wayne", "Batman"]
+
+puts superHeroes["Clark Kent"]
+
+superHeroes["Barry Allen"] = "Flash"
+
+superHeroines = Hash["Lisa Morel", "Aquagirl", "Betty Kane", "Batgirl"]
+
+superHeroes.update(superHeroines)
+
+superHeroes.each do |key, value|
+    puts key.to_s + " : " + value
+end
