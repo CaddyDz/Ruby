b87663ed32f7ddb2c5aa52ed33dada54f587d302
Caddy Dz <tbitw31@gmail.com>
2017-09-08 16:07:18 +0100

    Split & delete string functions
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- rubyOrNotRuby.rb
+++ rubyOrNotRuby.rb
@@ -9,19 +9,12 @@ full_name = "{first_name} #{middle_name} #{last_name}"
 
 puts "\"a\".equal?(\"a\") : " + ("a".equal?"a").to_s
 
-puts full_name.upcase
-puts full_name.downcase
-puts full_name.swapcase
+ puts full_name.delete("a")
 
-full_name = "       " + full_name
+ name_array = full_name.split(//)
 
-full_name = full_name.lstrip
-full_name = full_name.rstrip
-full_name = full_name.strip
+ puts name_array
 
-puts full_name.rjust(20, '.')
-puts full_name.ljust(20, '.')
-puts full_name.center(20, '.')
+ name_array = full_name.split(/ /)
 
-puts full_name.chop
-puts full_name.chomp('as')
+ puts name_array
