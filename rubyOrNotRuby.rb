e65a6ed895da8cdcba34690553e28c0e7efb7a1e
Caddy Dz <tbitw31@gmail.com>
2017-09-08 17:04:08 +0100

    Array functions
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- rubyOrNotRuby.rb
+++ rubyOrNotRuby.rb
@@ -3,4 +3,16 @@ array_2 = Array.new(5)
 array_3 = Array.new(5, "empty")
 array_4 = [1, "two", 3, 5.5]
 
-puts array_4[2, 2].join(", ")
+array_4.unshift(0)
+array_4.shift()
+
+array_4.push(100, 200)
+
+array_4.pop
+
+array_4.concat([10, 20, 30])
+
+puts "Array Size : " + array_4.size().to_s
+puts "Array Contains 100 : " + array_4.include?(100).to_s
+puts "How Many 100s : " + array_4.count(100).to_s
+puts "Array Empty : " + array_4.empty?.to_s
