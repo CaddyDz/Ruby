64ccf178ae6f4ea87127386a025889c49afbd7b2
Caddy Dz <tbitw31@gmail.com>
2017-09-08 12:55:24 +0100

    Load external code
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- rubyOrNotRuby.rb
+++ rubyOrNotRuby.rb
@@ -1,9 +1,1 @@
-write_handler = File.new("yourSum.out", "w")
-
-write_handler.puts("Random Text").to_s
-
-write_handler.close
-
-data_from_file = File.read("yourSum.out")
-
-puts "Data From File : " + data_from_file
+load "rubyOrNotRuby2.rb"
