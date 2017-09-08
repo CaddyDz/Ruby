3b368c474acfdb0f5301a4d66ec217c1ae186422
Caddy Dz <tbitw31@gmail.com>
2017-09-08 14:53:49 +0100

    Custom exceptions
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- rubyOrNotRuby.rb
+++ rubyOrNotRuby.rb
@@ -1,13 +1,11 @@
-print "Enter a Number : "
-first_num = gets.to_i
-print "Enter Another : "
-second_num = gets.to_i
+age = 12
+
+def check_age(age)
+    raise ArgumentError, "Enter Positive Number" unless age >= 0
+end 
 
 begin
-    answer = first_num / second_num
-rescue
-    puts "You can't divide by zero"
-    exit
+    check_age(-1)
+rescue ArgumentError
+    puts "That is an impossible age"
 end
-
-puts "#{first_num} / #{second_num} = #{answer}"
