825222486ae7438f9ff09f46c13618d3bab2ee57
Caddy Dz <tbitw31@gmail.com>
2017-11-01 13:01:50 +0100

    Celsuis to Fahrenheit conversion
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- p1/ch1/io.rb
+++
@@ -1,8 +0,0 @@
-# Get a line of keyboard input
-puts "What is your name?"
-name = gets
-puts "Hello " + name
-
-=begin
-You can assign the input line directly to a variable (the variable name in this example)
-=end
