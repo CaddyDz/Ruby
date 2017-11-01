73052346c8aaa92dcf5b455ac779f5d8c6f12125
Caddy Dz <tbitw31@gmail.com>
2017-11-01 12:34:30 +0100

    Basic input/output methods and flow control in Ruby
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

---
+++ p1/ch1/io.rb
@@ -0,0 +1,12 @@
+# Print something to the screen
+print "Hello"
+puts "Hello"
+x = "Hello"
+puts x
+x = "Hello"
+print x
+x = "Hello"
+p x
+=begin
+  puts adds a newline to the string it outputs if there isn’t one at the end already; print doesn’t. print prints exactly what it’s told to and leaves the cursor at the end. (Note: On some platforms, an extra line is automatically output at the end of a program.) p outputs an inspect string, which may contain extra information about what it’s printing.
+=end
