1c531edc896f548a3cccfe949e66e42ed1815bef
Salim Djerbouh <tbitw31@gmail.com>
2017-11-01 15:29:16 +0100

    File I/O
    Signed-off-by: Salim Djerbouh <tbitw31@gmail.com>

---
+++ p1/ch1/c2finout.rb
@@ -0,0 +1,11 @@
+puts "Reading data from data.txt"
+num = File.read("data.txt")
+celsius = num.to_i
+fahrenheit = (celsius * 9 / 5) + 32
+puts "The number is " + num
+puts "Enter file name to output data to"
+filename = gets.to_s
+puts "Saving result to output file " + filename
+fh = File.new(filename, "w")
+fh.puts fahrenheit
+fh.close
