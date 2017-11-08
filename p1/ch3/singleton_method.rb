ce551b9fd94ecac0d35e36cafc283fef20c57cb6
Caddy Dz <tbitw31@gmail.com>
2017-11-08 10:40:26 +0100

    Hotfix for singleton methods
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- p1/ch3/singleton_method.rb
+++ p1/ch3/singleton_method.rb
@@ -1,18 +1,26 @@
+# The ticket class is responsible for holding its attributes
 class Ticket
-    def initialize(venue, date)
-    end
-    attr_accessor :price
-    def Ticket.most_expensive(*tickets)
-        tickets.max_by(&:price)
-    end
+  attr_reader :venue, :date
+  # A class that publishes a setter for an instance variable invites client
+  # classes to become too intimate with its inner workings, and in particular
+  #  with its representation of state.
+  attr_accessor :price
+
+  def initialize(venue, date)
+    @venue = venue
+    @date = date
+  end
 end
 
-th = Ticket.new("Town Hall", "11/12/13")
-cc = Ticket.new("Convention Center", "12/13/14/")
-fg = Ticket.new("FAirgrounds", "13/14/15/")
+def Ticket.most_expensive(*tickets)
+  tickets.max_by(&:price)
+end
+
+th = Ticket.new('Town Hall', '11/12/13')
+cc = Ticket.new('Convention Center', '12/13/14/')
+fg = Ticket.new('Fairgrounds', '13/14/15/')
 th.price = 12.55
 cc.price = 10.00
 fg.price = 18.00
 highest = Ticket.most_expensive(th, cc, fg)
-puts "The highest-priced ticket is the one for #{highest.venue}."    
-
+puts "The highest-priced ticket is the one for #{highest.venue}."
