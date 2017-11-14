f6e4a068e0016bdc4e66acffa1f0b7f1ca3acef8
Caddy Dz <tbitw31@gmail.com>
2017-11-14 12:28:37 +0100

    Built-in Ruby classes with literal constructors
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

---
+++ p2/ch7/constructors/table.rb
@@ -0,0 +1,27 @@
+=begin  
+  Built-in Ruby classes with literal constructors
+=end
+
+# String
+"new string"
+'new string'
+
+# Symbol
+:symbol
+:"symbol with spaces"
+
+# Array
+[1,2,3,4,5]
+
+# Hash
+{"New York" => "NY", "Oregon" => "OR"}
+
+# Range
+0..9
+0...10
+
+# Regexp
+/([a-z]+)/
+
+# Proc (lambda)
+->(x,y) {x * y}
