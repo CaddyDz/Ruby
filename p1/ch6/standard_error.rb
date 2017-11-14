fa6e737bdd28ead43024949d7813fc8c32e0a080
Caddy Dz <tbitw31@gmail.com>
2017-11-14 11:40:02 +0100

    End Part 1
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

---
+++ p1/ch6/standard_error.rb
@@ -0,0 +1,16 @@
+class InvalidLineError < StandardError
+  
+end
+
+def line_from_file(filename, substring)
+  fh = File.open(filename)
+  line = fh.gets
+  raise InvalidLineError unless line.include?(substring)
+  return line
+
+  rescue InvalidLineError
+    puts "Invalid line!"
+    raise
+  ensure
+    fh.close
+end
