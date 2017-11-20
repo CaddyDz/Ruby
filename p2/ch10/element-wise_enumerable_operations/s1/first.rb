7538e80fdb8ab8494e34adee9804b63bec67dc61
Caddy Dz <tbitw31@gmail.com>
2017-11-20 10:47:04 +0100

    Element-wise enumerable operations The first method
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

---
+++ p2/ch10/element-wise_enumerable_operations/s1/first.rb
@@ -0,0 +1,7 @@
+puts [1,2,3,4].first
+puts (1..0).first
+first = {
+    1 => 2,
+    "one" => "two"
+}.first
+p first
---
+++ p2/ch10/element-wise_enumerable_operations/s1/trois.rb
@@ -0,0 +1,4 @@
+hash = { 3 => "three", 1 => "one", 2 => "two"}
+p hash.first
+hash[3] = "trois"
+p hash.first
