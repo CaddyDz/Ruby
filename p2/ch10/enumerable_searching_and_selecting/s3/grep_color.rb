c6e6791175a435934e30f4db26de83d197f7e4e5
Caddy Dz <tbitw31@gmail.com>
2017-11-20 10:16:20 +0100

    Selecting on threequal matches with grep
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

---
+++ p2/ch10/enumerable_searching_and_selecting/s3/grep_color.rb
@@ -0,0 +1,2 @@
+colors = %w{ red orange yellow green blue indigo violet }
+puts colors.grep(/o/) {|color| color.capitalize}
---
+++ p2/ch10/enumerable_searching_and_selecting/s3/miscellany.rb
@@ -0,0 +1,3 @@
+miscellany = [75, "hello", 10...20, "goodbye"]
+puts miscellany.grep(String)
+puts miscellany.grep(50..100)
