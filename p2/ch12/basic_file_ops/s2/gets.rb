6f5b72a7606e6906d25cf3f521c5004d9cdc0a8c
Salim Djerbouh <tbitw31@gmail.com>
2017-11-24 17:03:58 +0100

    Byte and character based file reading
    Signed-off-by: Salim Djerbouh <tbitw31@gmail.com>

---
+++ p2/ch12/basic_file_ops/s2/gets.rb
@@ -0,0 +1,7 @@
+f = File.new("/home/caddy/Documents/Ruby/p2/ch12/basic_file_ops/s1/ticket.rb")
+puts f.gets
+puts f.read
+puts f.gets
+puts f.readline
+puts f.rewind
+puts f.readlines
---
+++ p2/ch12/basic_file_ops/s3/getc.rb
@@ -0,0 +1,8 @@
+f = File.new("/home/caddy/Documents/Ruby/p2/ch12/basic_file_ops/s1/ticket.rb")
+p f.getc
+f.ungetc("X")
+p f.gets
+p f.getc
+p f.readchar
+p f.getbyte
+p f.readbyte
